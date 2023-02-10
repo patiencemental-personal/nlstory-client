1d1c8c77fcf4bca2aefc56110e6de7d5e5a6e253
patiencemental <ejsvk3284@kakao.com>
2023-02-10 15:47:09 +0900

    Spinner 컴포넌트 추가

---
+++ src/components/common/Spinner.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className='w-20 h-20 bg-slate-800 animate-spin rounded-full border-slate-800 border-b-sgnr-blue border-2 mx-auto'></div>
+  )
+}
