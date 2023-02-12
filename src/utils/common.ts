db991d9f810b35306fbc6ce5f3299b6cf9015bc0
patiencemental <ejsvk3284@kakao.com>
2023-02-12 20:07:31 +0900

    공통 함수 추가 betweenNumber

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,3 +3,7 @@ import { v4 as uuid } from 'uuid';
 export function generateId(): string {
   return uuid();
 }
+
+export function betweenNumber(num: number, min: number, max: number): boolean {
+  return num >= min && num <= max;
+}
